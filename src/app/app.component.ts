0c540790f64f5d727196d665f48998cda96b678e
pranav <pranavverma550@gmail.com>
2019-04-21 08:15:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPipeString';
+  name:string="Rahul";
+  postion:string="software developer"
+  work:string="software development"
 }
